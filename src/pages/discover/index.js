5361e04a235b870c9a3624d70603e817c63428a2
e <lichenxin@insighters.com>
2020-08-24 18:36:18 +0800

    发现音乐页面初始化和引入axios

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,9 +1,33 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
 
-export default memo(function Discover() {
+import { DiscoverWrapper, TopMenu } from './style'
+import request from '@/services/request'
+
+import { dicoverMenu } from '@/common/local-data'
+
+
+export default memo(function Discover(props) {
+  useEffect(() => {
+    request({
+      url: "/banner"
+    }).then(console.log)
+  })
   return (
-    <div>
-      这里是发现内容
-    </div>
+    <DiscoverWrapper>
+      <div className="top">
+        <TopMenu className="wrap-v1">
+          {
+            dicoverMenu.map((item, index) => {
+              return (<div className="item" key={item.title}>
+                <NavLink to={item.link}>{item.title}</NavLink>
+              </div>)
+            })
+          }
+        </TopMenu>
+        {renderRoutes(props.route.routes)}
+      </div>
+    </DiscoverWrapper>
   )
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,75 @@
+import React from 'react';
+import { Redirect } from "react-router-dom";
+
 import Discover from "../pages/discover";
+import Recommend from '../pages/discover/c-pages/recommend'
+import Ranking from '../pages/discover/c-pages/ranking'
+import Songs from '../pages/discover/c-pages/songs'
+import Djradio from '../pages/discover/c-pages/djradio'
+import Artist from '../pages/discover/c-pages/artist'
+import Album from '../pages/discover/c-pages/album'
+import Player from '../pages/discover/c-pages/player'
 import Friend from "../pages/friend";
 import Mine from "../pages/mine";
 
-const routes =[
+
+const routes = [
   {
-    path:"/",
-    exact:true,
-    component:Discover
+    path: "/",
+    exact: true,
+    render: () => (
+      <Redirect to="/discover" />
+    )
   },
   {
-    path:"/mine",
-    component:Mine
+    path: "/discover",
+    component: Discover,
+    routes: [
+      {
+        path: "/discover",
+        exact: true,
+        render: () => (
+          <Redirect to={"/discover/artist"} />
+        )
+      },
+      {
+        path: "/discover/recommend",
+        component: Recommend
+      },
+      {
+        path: "/discover/ranking",
+        component: Ranking
+      },
+      {
+        path: "/discover/songs",
+        component: Songs
+      },
+      {
+        path: "/discover/djradio",
+        exact: true,
+        component: Djradio
+      },
+      {
+        path: "/discover/artist",
+        component: Artist
+      },
+      {
+        path: "/discover/album",
+        component: Album
+      },
+      {
+        path: "/discover/player",
+        component: Player
+      }
+    ]
   },
   {
-    path:"/friend",
-    component:Friend
+    path: "/mine",
+    component: Mine
+  },
+  {
+    path: "/friend",
+    component: Friend
   },
 
 
